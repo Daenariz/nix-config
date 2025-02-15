2f63ab34194cfaf81ad2735522ced54ced771a49
susagi <susagi@sid.ovh>
2025-02-15 14:28:00 +0100

    librewolf policy added

--- users/susagi/home/hyprland/programs.nix
+++ users/susagi/home/hyprland/programs.nix
@@ -15,17 +15,24 @@
       ttrss-passwordfile "${config.sops.secrets.tt-rss.path}"
     '';
   };
+
   programs = {
     kitty.settings.background_opacity = lib.mkForce "0.5";
     # transparent nixvim plugin, only works with styling.scheme disabled since background color might be overwritten I guess
     nixvim.plugins.transparent.enable = lib.mkForce false;
 
-    librewolf.profiles.default.extensions = lib.mkAfter (
-      with inputs.nur.legacyPackages."${pkgs.system}".repos.rycee.firefox-addons;
-      [
-        bitwarden
-      ]
-    );
+    librewolf = {
+      policies.Homepage.StartPage = lib.mkForce "previous-session";
+      profiles.default = {
+        extensions = lib.mkAfter (
+          with inputs.nur.legacyPackages."${pkgs.system}".repos.rycee.firefox-addons;
+          [
+            bitwarden
+          ]
+        );
+      };
+    };
+
     ssh.addKeysToAgent = "yes";
   };
 }
