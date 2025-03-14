75fe63cf7e6f4d04594447d4cbdfb0887c75bc4b
susagi <susagi@negitorodon.de>
2025-03-14 20:13:40 +0100

    home assistant config to futro added

---
+++ hosts/futro/homeass.nix
@@ -0,0 +1,51 @@
+{
+  inputs,
+  config,
+  outputs,
+  lib,
+  ...
+}:
+
+{
+  imports = [
+    inputs.core.nixosModules.openssh
+    inputs.core.nixosModules.nginx
+    # inputs.core.nixosModules.open-webui
+    # outputs.nixosModules.vaultwarden
+    inputs.core.nixosModules.rss-bridge
+    inputs.core.nixosModules.tt-rss
+  ];
+
+ # services.vaultwarden = {
+  #   enable = true;
+  #   subdomain = "vault";
+  # };
+  services.nginx.enable = true;
+  services.openssh = {
+    enable = true;
+    ports = [ 3407 ];
+  };
+  # services.open-webui.enable = true;
+  services.rss-bridge = {
+    enable = true;
+    dataDir = "/data/rss-bridge";
+    subdomain = "rss-bridge";
+  };
+  services.tt-rss = {
+    enable = true;
+    root = "/data/tt-rss";
+  };
+  services.home-assistant = {
+    enable = true;
+    extraComponents = [
+      "analytics"
+      "google_translate"
+      "met"
+      "radio_browser"
+      "shopping_list"
+    ];
+    config = {
+      default_config = {};
+    };
+  };
+}
