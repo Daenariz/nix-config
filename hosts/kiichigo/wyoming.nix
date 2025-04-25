4e71bec538fa599216ee2798821e5ea820e28f9b
susagi <susagi@negitorodon.de>
2025-04-25 15:51:32 +0200

    wyoming wide bool added

--- hosts/kiichigo/wyoming.nix
+++ hosts/kiichigo/wyoming.nix
@@ -1,19 +1,23 @@
-{
-services.wyoming = {
-  faster-whisper.servers = {
-    testserver = {
-      enable = true;
-      uri = "tcp://0.0.0.0:10300";
-      language = "auto";
-    };
-  };
 
-  piper.servers = {
-    testserver = {
-      enable = true;
-      uri = "tcp://0.0.0.0:10200";
-      voice = "de_DE-thorsten-high";
+let
+  enableWyoming = false;
+in
+{
+  services.wyoming = {
+    faster-whisper.servers = {
+      testserver = {
+        enable = enableWyoming;
+        uri = "tcp://0.0.0.0:10300";
+        language = "auto";
+      };
+    };
+
+    piper.servers = {
+      testserver = {
+        enable = enableWyoming;
+        uri = "tcp://0.0.0.0:10200";
+        voice = "de_DE-thorsten-high";
+      };
     };
   };
-};
 }
