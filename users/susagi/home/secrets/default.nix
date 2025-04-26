a7a25282f85366907aa143aed115fe235c522691
susagi <susagi@sid.ovh>
2025-04-26 11:20:21 +0200

    gh token added

--- users/susagi/home/secrets/default.nix
+++ users/susagi/home/secrets/default.nix
@@ -1,4 +1,11 @@
+{config, ...}:
+
 {
   sops.secrets.nextcloud = { };
   sops.secrets.tt-rss = { };
+
+  sops.secrets.github-token = { };
+  sops.templates.access-tokens.content = ''
+    access-tokens = github.com=${config.sops.placeholder.github-token}
+  '';
 }
