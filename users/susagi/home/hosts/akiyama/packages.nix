fe06a3fe26b54b8b7b79d796132a9274fb02fc4b
susagi <susagi@sid.ovh>
2025-05-11 09:34:36 +0200

    akiyama game-specific packages added

---
+++ users/susagi/home/hosts/akiyama/packages.nix
@@ -0,0 +1,41 @@
+{ pkgs, ... }:
+
+with pkgs;
+
+[
+]
+++
+  # ai stuff
+  [
+  #  tts
+  #  ollama
+  #  aichat
+  #    jan
+  ]
+++
+  #　たいせつな日本語の勉強
+  [
+    tagainijisho # タガイニ辞書
+    anki
+    tango
+    noto-fonts-cjk-sans # ファイアフォックスでは、日本語のかなを見せるのために
+  ]
+++
+  # testing
+  [
+    tidal-hifi  # TODO: need to check autostart, crashing
+
+  #  bitwarden-cli
+  #   bitwarden-desktop
+    bitwarden-menu
+  # github stuff
+  ]
+++
+  # essentials
+  [
+    steam
+    vesktop
+    signal-desktop
+  #stable.whatsapp-for-linux #no unstable, stable not working
+  #    zoom-us # SSO doesn't seem too work https://github.com/NixOS/nixpkgs/issues/73532 (?)
+  ]
