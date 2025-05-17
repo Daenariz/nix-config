a9d2aa93298bab11d89bbf704421667bca4e512f
Daenariz <deniz.onlinegames@gmail.com>
2025-05-17 14:01:03 +0200

    settings imported

---
+++ users/neo/home/hyprland/settings/hyprland.nix
@@ -0,0 +1,40 @@
+{
+  animations.enabled = true;
+  #gestures.workspace_swipe = true;
+  # probably changed in 24.11
+  # decoration = {
+  #   blur.enabled = true;
+  #   shadow.enabled = true;
+  # };
+  exec-once = [
+    "fcitx5 -d -r"   # https://nixos.wiki/wiki/Fcitx5 for Hyprland usage
+    "fcitx5-remote -r"
+    #    "[workspace 1] kitty"
+    "[workspace 2 silent] librewolf"
+    #"[workspace 4 silent] thunderbird"
+    "[workspace 5 silent] element-desktop"
+    "[workspace 5 silent] signal-desktop"
+    "[workspace 6 silent] tidal-hifi"
+  ];
+
+  windowrulev2 = [
+    "monitor   1, class:^librewolf$"
+    "workspace 2, class:^librewolf$"
+
+    "monitor   0, class:^Element$, title:^Element"
+    "workspace 5, class:^Element$, title:^Element"
+
+    #"monitor   0, class:^thunderbird$, title:Thunderbird$"
+    #"workspace 4, class:^thunderbird$, title:Thunderbird$"
+
+    "monitor   0, class:^signal$, title:^Signal"
+    "workspace 5, title:^Signal"
+
+    "monitor   0, class:^tidal-hifi$, title:^tidal-hifi"
+    "workspace 6, title:^TIDAL"
+  ];
+  workspace = [
+    #    "r[2], monitor:DP-1, default:true"
+    #"r[3], monitor:"
+  ];
+}
---
+++ users/neo/home/hyprland/settings/waybar.nix
@@ -0,0 +1,47 @@
+{
+      mainBar = {
+        modules-left = [
+          "hyprland/workspaces"
+          "tray"
+         "keyboard-state"
+      #    "hyprland/language"
+          "hyprland/window"
+        ];
+        modules-right = [
+          "custom/timer"
+          "custom/newsboat"
+          "bluetooth"
+          "network"
+          "cpu"
+          "memory"
+          "disk"
+          "pulseaudio#input"
+          "pulseaudio#output"
+      #"battery"
+        ];
+
+        "hyprland/workspaces" = {
+          format = " {icon} ";
+          format-icons = {
+            "1" = "";  # default
+            "2" = "";  # browser
+            "3" = "";  # discord
+            "4" = "";  # mail
+            "5" = "";  # chats
+            "6" = "";  # music
+         #   "7" = "";  # vm
+            urgent = "";
+            active = "";
+            default = "";
+          };
+
+          tray = {
+            icon-size = 21;
+            spacing = 10;
+          };
+
+          all-outputs = false;
+          persistent-workspaces."*" = 7;
+        };
+  };
+}
