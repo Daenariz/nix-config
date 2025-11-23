e1880023540c108c24358dc64f8b66d1f35003d4
susagi <susagi@sid.ovh>
2025-11-23 17:05:32 +0100

    oci tests

--- hosts/futro/services.nix
+++ hosts/futro/services.nix
@@ -2,6 +2,7 @@
   inputs,
   outputs,
   config,
+  lib,
   ...
 }:
 
@@ -13,8 +14,16 @@
     inputs.core.nixosModules.mailserver
     inputs.core.nixosModules.matrix-synapse
     inputs.core.nixosModules.nextcloud
+    inputs.core.nixosModules.headscale
   ];
 
+  services.headscale = {
+    enable = true;
+    settings.policy.path = lib.mkForce "/home/susagi/backups/acl.hujson";
+    openFirewall = true;
+   # subdomain = "head";
+  };
+  
   services.uptime-kuma.enable = true;
 
   services.nginx.virtualHosts."kuma.negitorodon.de" = {
--- hosts/kyoujin/ollama.nix
+++ hosts/kyoujin/ollama.nix
@@ -2,8 +2,13 @@
 {
   imports = [
     #inputs.core.nixosModules.open-webui
-    inputs.nixified-ai.nixosModules.comfyui
+#    inputs.nixified-ai.nixosModules.comfyui
+     inputs.core.nixosModules.open-webui-oci
   ];
+
+  services.open-webui-oci.enable = true;
+  services.open-webui-oci.enableSignUp = true;
+
   services.ollama = {
     enable = true;
     host = "0.0.0.0";
@@ -18,37 +23,37 @@
   };
 
   
-services.comfyui = {
-    enable = false;
-    package = inputs.nixified-ai.packages.${pkgs.system}.comfyui-nvidia; # package in overlay?
+#services.comfyui = {
+#    enable = false;
+#    package = inputs.nixified-ai.packages.${pkgs.system}.comfyui-nvidia; # package in overlay?
 #    user = "comfyui";
  #   group = "comfyui";
-    host = "192.168.178.107";
-    extraFlags = [
-    "--lowvram"
-    ];
-    openFirewall = true;
-    acceleration = "cuda";
-#    port = 8187;
-    customNodes = with pkgs.comfyuiPackages; [
-      comfyui-gguf
-      comfyui-impact-pack
-      comfyui-workflow-templates
-      comfyui-advanced-controlnet
-      comfyui-easy-use
+#    host = "192.168.178.107";
+#    extraFlags = [
+#    "--lowvram"
+#    ];
+#    openFirewall = true;
+#    acceleration = "cuda";
+##    port = 8187;
+#    customNodes = with pkgs.comfyuiPackages; [
+#      comfyui-gguf
+#      comfyui-impact-pack
+#      comfyui-workflow-templates
+#      comfyui-advanced-controlnet
+#      comfyui-easy-use
 #      comfyui-essentials
-    ];
-    models = with pkgs.nixified-ai.models; [
-    flux1-dev-q4_0 
-    (pkgs.fetchair {
-                name = "realvisxlV50_v50LightningBakedvae.safetensors";
-                air = "urn:air:sdxl:checkpoint:civitai:139562@798204";
-                sha256 = "FABCADD9330DCC4F9702063428D40B9D4D07168D8ACEFC819B8D1D9DB466B3EC";
-                passthru = {
-                  comfyui.installPaths = [ "checkpoints" ];
-                };
-              })];
-  };
+#    ];
+#    models = with pkgs.nixified-ai.models; [
+#    flux1-dev-q4_0 
+#    (pkgs.fetchair {
+#                name = "realvisxlV50_v50LightningBakedvae.safetensors";
+#                air = "urn:air:sdxl:checkpoint:civitai:139562@798204";
+#                sha256 = "FABCADD9330DCC4F9702063428D40B9D4D07168D8ACEFC819B8D1D9DB466B3EC";
+#                passthru = {
+#                  comfyui.installPaths = [ "checkpoints" ];
+#                };
+#              })];
+#  };
 
   # services.open-webui = {
   #enable = true;
