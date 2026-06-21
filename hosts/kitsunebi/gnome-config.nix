37d28dc8b354ea310da21144d836b4794fcfa5d7
Daenariz <152905366+Daenariz@users.noreply.github.com>
2026-06-21 19:18:43 +0200

    Comment out network and locale settings
    Comment out various configurations in gnome-config.nix.

--- hosts/kitsunebi/gnome-config.nix
+++ hosts/kitsunebi/gnome-config.nix
@@ -5,42 +5,30 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  # networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "Europe/Berlin";
+  # time.timeZone = "Europe/Berlin";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  #i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
+  #i18n.extraLocaleSettings = {
+  #  LC_ADDRESS = "de_DE.UTF-8";
+  #  LC_IDENTIFICATION = "de_DE.UTF-8";
+  #  LC_MEASUREMENT = "de_DE.UTF-8";
+  #  LC_MONETARY = "de_DE.UTF-8";
+  #  LC_NAME = "de_DE.UTF-8";
+  #  LC_NUMERIC = "de_DE.UTF-8";
+  #  LC_PAPER = "de_DE.UTF-8";
+  #  LC_TELEPHONE = "de_DE.UTF-8";
+  #  LC_TIME = "de_DE.UTF-8";
+  #};
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -80,28 +68,11 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users."neo" = {
-    isNormalUser = true;
-    description = "neo";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
-  };
-
   # Install firefox.
   programs.firefox.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
